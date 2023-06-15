6c4003489dc85387b6e82eb93c7116bc51a28833
Eyob2-7 <eyoba4456@gmail.com>
2023-06-15 16:50:33 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,29 +1,15 @@
 #include <stdio.h>
-#include <inttypes.h>
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ *
+ * Return: 0
  */
 int main(void)
 {
-	if (sizeof(void *) == 4)
-	{
-		printf("Running on a 32-bit machine\n");
-		printf("Size of a pointer: %zu byte(s)\n", sizeof(void *));
-		printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-		printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	}
-	else if (sizeof(void *) == 8)
-	{
-		printf("Running on a 64-bit machine\n");
-		printf("Size of a pointer: %zu byte(s)\n", sizeof(void *));
-		printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
-		printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	}
-	else
-	{
-		printf("Unknown machine architecture\n");
-	}
-
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
