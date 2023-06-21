d4cb2194c88113e2b8c95f4cbef8497e37de1e82
Eyob2-7 <eyoba4456@gmail.com>
2023-06-20 18:19:55 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,36 +6,71 @@
  */
 void print_to_98(int n)
 {
-	int i;
+    int i;
+    char digit;
 
-	if (n <= 98)
-	{
-		for (i = n; i <= 98; i++)
-		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
+    if (n <= 98)
+    {
+        for (i = n; i <= 98; i++)
+        {
+            if (i < 0)
+            {
+                _putchar('-');
+                digit = '0' - (i % 10);
+            }
+            else
+            {
+                digit = '0' + (i % 10);
+            }
 
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
-	else
-	{
-		for (i = n; i >= 98; i--)
-		{
-			_putchar(i / 10 + '0');
-			_putchar(i % 10 + '0');
+            if (i / 10 != 0)
+            {
+                _putchar('0' + (i / 10));
+                _putchar(digit);
+            }
+            else
+            {
+                _putchar(digit);
+            }
 
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
-		}
-	}
+            if (i != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+    else
+    {
+        for (i = n; i >= 98; i--)
+        {
+            if (i < 0)
+            {
+                _putchar('-');
+                digit = '0' - (i % 10);
+            }
+            else
+            {
+                digit = '0' + (i % 10);
+            }
 
-	_putchar('\n');
+            if (i / 10 != 0)
+            {
+                _putchar('0' + (i / 10));
+                _putchar(digit);
+            }
+            else
+            {
+                _putchar(digit);
+            }
+
+            if (i != 98)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+    }
+
+    _putchar('\n');
 }
