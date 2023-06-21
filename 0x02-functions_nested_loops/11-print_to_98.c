7cc9e176f1cee9e349d19721e90dcaf968b3ed50
Eyob2-7 <eyoba4456@gmail.com>
2023-06-20 18:11:02 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,31 +2,40 @@
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98.
- * @n: Starting number.
+ * @n: The starting number.
  */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n <= 98)
 	{
-		for (; n < 98; n++)
+		for (i = n; i <= 98; i++)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	else
 	{
-		for (; n > 98; n--)
+		for (i = n; i >= 98; i--)
 		{
-			_putchar(n / 10 + '0');
-			_putchar(n % 10 + '0');
-			_putchar(',');
-			_putchar(' ');
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
-	_putchar('9');
-	_putchar('8');
+
 	_putchar('\n');
 }
