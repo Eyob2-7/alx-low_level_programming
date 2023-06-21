33db2d6a70f98c87e713f314dcfa9ecbd8b1879d
Eyob2-7 <eyoba4456@gmail.com>
2023-06-20 18:24:35 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,76 +1,40 @@
 #include "main.h"
 
 /**
- * print_to_98 - Prints all natural numbers from n to 98.
- * @n: The starting number.
+ * print_to_98 - function that prints all natural numbers from n to 98
+ * @n: takes in an integer
  */
 void print_to_98(int n)
 {
-    int i;
-    char digit;
+	int i;
 
-    if (n <= 98)
-    {
-        for (i = n; i <= 98; i++)
-        {
-            if (i < 0)
-            {
-                _putchar('-');
-                digit = '0' - (i % 10);
-            }
-            else
-            {
-                digit = '0' + (i % 10);
-            }
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 
-            if (i / 10 != 0)
-            {
-                _putchar('0' + (i / 10));
-                _putchar(digit);
-            }
-            else
-            {
-                _putchar(digit);
-            }
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-        {
-            if (i < 0)
-            {
-                _putchar('-');
-                digit = '0' - (i % 10);
-            }
-            else
-            {
-                digit = '0' + (i % 10);
-            }
-
-            if (i / 10 != 0)
-            {
-                _putchar('0' + (i / 10));
-                _putchar(digit);
-            }
-            else
-            {
-                _putchar(digit);
-            }
-
-            if (i != 98)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-        }
-    }
-
-    _putchar('\n');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
 }
