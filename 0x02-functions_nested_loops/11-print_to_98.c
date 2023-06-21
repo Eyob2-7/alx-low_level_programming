869b8375995fb75ab8e9f19f73a085f10d1dc01c
Eyob2-7 <eyoba4456@gmail.com>
2023-06-20 18:50:36 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,28 @@
 #include "main.h"
-#include "holberton.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_number - prints a number
+ * @n: The number to be printed
+ */
+void print_number(int n)
+{
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + (n % 10));
+}
 
 /**
  * print_to_98 - function that prints all natural numbers from n to 98
@@ -7,37 +30,25 @@
  */
 void print_to_98(int n)
 {
-	int i;
-
-	if (n > 98)
+	if (n <= 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (; n < 98; n++)
 		{
-			_putchar(i / 100 + '0');
-			_putchar((i / 10) % 10 + '0');
-			_putchar(i % 10 + '0');
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			print_number(n);
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	else
 	{
-		for (i = n; i <= 98; i++)
+		for (; n > 98; n--)
 		{
-			_putchar(i / 100 + '0');
-			_putchar((i / 10) % 10 + '0');
-			_putchar(i % 10 + '0');
-
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			print_number(n);
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
+
+	print_number(n);
 	_putchar('\n');
 }
