f129cc994c16781541701fb712e3f790a4a98160
Eyob2-7 <eyoba4456@gmail.com>
2023-06-22 14:01:36 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,14 @@
 void more_numbers(void)
 {
 	int i, j;
-	char digits[] = "01234567891011121314";
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 17; j++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar(digits[j]);
+			if (j > 9)
+				_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
